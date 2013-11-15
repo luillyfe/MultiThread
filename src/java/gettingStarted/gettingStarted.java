86197de942f35caacc95f8d0048e00d23aeeea43
luillyfe <luillyfe89@gmail.com>
2013-11-15 18:58:43 -0500

    Intervalos de 4 secundos

--- src/java/gettingStarted/gettingStarted.java
+++ src/java/gettingStarted/gettingStarted.java
@@ -4,8 +4,12 @@
  */
 package gettingStarted;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
- * Mi primera aplicacion Multihilo 
+ * Imprimir mensajes en intervalos de cuatro segundos.
+ * Se utiliza el metodo 'sleep' para pausar la ejecucion del hilo.
  * @author luilly
  */
 public class gettingStarted implements Runnable {
@@ -19,6 +23,22 @@ public class gettingStarted implements Runnable {
 
     @Override
     public void run() {
-        System.out.println("Hola, soy un hilo");
+        String[] informacion = {"El ciervo alimenta sus crias.",
+                                "El león se abalancha sobre el ciervo.",
+                                "El cazador se da cuenta de la carrera.",
+                                "El ciervo piensa, si el león no me alcanza"
+                              + "¿Volvera y se comera a ms hijos?.",
+                                "Precisamente el león esta pensado, ¿Por que "
+                     + "cansarme con la madre si puedo sin ningun esfuerzo?.",
+               "El cazador piensa si mato al león, tendre un exelente trofeo,"
+    + "pero si mato al ciervo, tendre trofeo y ademas podre comerme su carne.",
+        "De repente todos se miran, con la presión del primer movimiento."};
+        
+        for(String i: informacion) {
+            try {
+                Thread.sleep(4000);
+            } catch (InterruptedException ex) { System.err.println(ex); }
+            System.out.println(i);
+        }
     }
 }

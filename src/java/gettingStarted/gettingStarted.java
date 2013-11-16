1fbd9c41e42745f353e296805e1cd4df2682cde5
luillyfe <luillyfe89@gmail.com>
2013-11-15 23:51:38 -0500

    Ejecucion multihiloen forma

--- src/java/gettingStarted/gettingStarted.java
+++ src/java/gettingStarted/gettingStarted.java
@@ -4,12 +4,12 @@
  */
 package gettingStarted;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 /**
- * Imprimir mensajes en intervalos de cuatro segundos.
- * Se utiliza el metodo 'sleep' para pausar la ejecucion del hilo.
+ * Esta la primera aplicacion mulithilo en forma, consta de dos hilos:
+ * el principal y natural, y un segundo que imprime una serie de mensajes.
+ * Enviamos una señal de interrupcion al hilo que imprime los mensajes
+ * desde el hilo principal - el main -. Pero no terminamos su ejecucion
+ * sino que le permitimos continuar con su vida.
  * @author luilly
  */
 public class gettingStarted implements Runnable {
@@ -17,8 +17,42 @@ public class gettingStarted implements Runnable {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        new Thread(new gettingStarted()).start();
+    public static void main(String[] args) throws InterruptedException {
+        /** Para ejecutar desde netbeans, sino eliminar las siguientes dos
+            lineas de codigo!! */
+        args = new String[1];
+        args[0] = "6";
+        
+        /** Tiempo de espera hasta interrumpir la ejecución del hilo
+            - este tiempo es por defecto, es decir sino se especifica otro - */
+        long paciencia = 1000 * 60 * 60;
+        
+        /** Cambiamos el tiempo de espera por defecto  */
+        if(args.length>0) {
+            try {
+                paciencia = Long.parseLong(args[0]) * 1000;
+            } catch(NumberFormatException nfe) { 
+                System.err.println("Ingresa un numero valido!"); 
+                System.exit(1); }
+        }
+        
+        /** Lanzammos el hilo y registramo su tiempo de inicio */
+        long startTime = System.currentTimeMillis();
+        Thread t = new Thread(new gettingStarted());
+        t.start();
+        
+        /** Mientras el hilo esté aun en ejecucion...
+            mandamos una señal de interrupcion al hilo
+            'getingStarted' */
+        while(t.isAlive()) {
+            t.join(1000);
+            
+            if( (System.currentTimeMillis() - startTime) > paciencia &&
+                    t.isAlive() ) {
+                t.interrupt();
+                t.join();
+            }
+        }
     }
 
     @Override
@@ -37,7 +71,8 @@ public class gettingStarted implements Runnable {
         for(String i: informacion) {
             try {
                 Thread.sleep(4000);
-            } catch (InterruptedException ex) { System.err.println(ex); }
+            } catch (InterruptedException ie) { 
+                System.err.println("No habia termiando!"); }
             System.out.println(i);
         }
     }
